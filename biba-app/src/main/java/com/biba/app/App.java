067cbaf311efadee598793e977d47809f83d69c5
Elico Elric <theelico@gmail.com>
2022-05-09 17:15:36 +0300

    init

---
+++ biba-app/src/main/java/com/biba/app/App.java
@@ -0,0 +1,13 @@
+package com.biba.app;
+
+/**
+ * Hello world! but weeb
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hai Worurudu!" );
+    }
+}
